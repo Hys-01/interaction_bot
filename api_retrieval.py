59adbe7b103910b8b4619578127708d030484fdc
Brian Hwang <hbros2004@gmail.com>
2024-05-23 21:23:44 +1000

    completed HFAPIClient.ask_server()
    tried to create object and call to server but seems to be errors rn

--- api_retrieval.py
+++ api_retrieval.py
@@ -1,5 +1,7 @@
 import requests
 
+import os 
+from dotenv import load_dotenv
 # prototyping
 
 #requests.post()   # sends a post HTTP request to GET data WITH something
@@ -9,13 +11,25 @@ we should be able to choose either openAI api or huggingface api, code shouldnt 
 hmmm
 
 '''
-API_KEY = ""
+load_dotenv()
+
+
 #return = requests.post(url=API_KEY, )
 
 class HuggingFaceAPIClient:
     def __init__(self, api_key, model):
         self.key = api_key
-        self.model_url = model
+        self.model_url = f"https://api-inference.huggingface.co/models/{model}"
     
-    def ask_server():
-        pass
+    def ask_server(self, payload):
+        headers = {"Authorization": f"Bearer {self.key}"}
+        response = requests.post(self.model_url, headers=headers, json=payload)
+        return response.json()
+    
+payload_1 = "Hello, World!"
+HF_api_key = os.getenv('HF_API_KEY')
+print(HF_api_key)
+HF_model = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+HF_Client = HuggingFaceAPIClient(HF_api_key, HF_model)
+
+print(HF_Client.ask_server(payload_1))
