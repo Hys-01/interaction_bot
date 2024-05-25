d3df800b38cf92c430f75b7506a213aeb7c444e5
Hys-01 <hbros2004@gmail.com>
2024-05-25 23:44:26 +1000

    remove login from load

--- mistral-7b/call.py
+++ mistral-7b/call.py
@@ -1,14 +1,17 @@
 from transformers import pipeline
 from huggingface_hub import snapshot_download
 
+#doc had:
+#{"role": "system", "content": "You are a chatbot who always responds in a happy tone"},
+# as first item in messages, but model seems to only want user/assistant/user/assitant... format. 
 messages = [
-    {"role": "system", "content": "You are a chatbot who always responds in a happy tone"},
+
     {"role": "user", "content": "Who are you?"},
     {"role": "assistant", "content": "I am Mistral!"},
     {"role": "user", "content": "Hi Mistral."},
     {"role": "assistant", "content": "Nice to meet you."},
 ]
-snapshot_download(repo_id="mistralai/Mistral-7B-Instruct-v0.3", allow_patterns=["params.json", "consolidated.safetensors", "tokenizer.model.v3"], local_dir=mistral_models_path)
-chatbot = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
+
+chatbot = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3",max_new_tokens=50)
 response = chatbot(messages)
 print(response)
--- mistral-7b/load.py
+++ mistral-7b/load.py
@@ -6,8 +6,8 @@ from transformers import pipeline
 
 
 
-HF_api_key = os.getenv('HF_API_KEY')
-login(token = HF_api_key)
+#HF_api_key = os.getenv('HF_API_KEY')
+#login(token = HF_api_key)
 
 # initial download of model, it will be at C:\Users\[me]\mistral_models
 mistral_models_path = Path.home().joinpath('mistral_models', '7B-Instruct-v0.3')
@@ -17,11 +17,8 @@ mistral_models_path.mkdir(parents=True, exist_ok=True)
 snapshot_download(repo_id="mistralai/Mistral-7B-Instruct-v0.3", allow_patterns=["params.json", "consolidated.safetensors", "tokenizer.model.v3"], local_dir=mistral_models_path)
 
 
+# putting the pipeline call here doesnt make any differece
+# login isnt required anymore, since we stored the key somewehere i forgot 
 
 
-messages = [
-    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
-    {"role": "user", "content": "Who are you?"},
-]
-chatbot = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
-chatbot(messages)
+
