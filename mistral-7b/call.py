f6f945489a9463d80c771b3d8711e6aeb958b2e0
Hys-01 <hbros2004@gmail.com>
2024-05-25 23:19:31 +1000

    transformers pipeline attempt not working

--- mistral-7b/call.py
+++ mistral-7b/call.py
@@ -1,8 +1,14 @@
 from transformers import pipeline
+from huggingface_hub import snapshot_download
 
 messages = [
-    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "system", "content": "You are a chatbot who always responds in a happy tone"},
     {"role": "user", "content": "Who are you?"},
+    {"role": "assistant", "content": "I am Mistral!"},
+    {"role": "user", "content": "Hi Mistral."},
+    {"role": "assistant", "content": "Nice to meet you."},
 ]
+snapshot_download(repo_id="mistralai/Mistral-7B-Instruct-v0.3", allow_patterns=["params.json", "consolidated.safetensors", "tokenizer.model.v3"], local_dir=mistral_models_path)
 chatbot = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
-chatbot(messages)
+response = chatbot(messages)
+print(response)
