d96ad53e96a1de2a23db68e55d54bdf8c23b0695
Hys-01 <hbros2004@gmail.com>
2024-06-06 23:34:30 +1000

    updated test with drafts
    testing ways to make faster

---
+++ blenderbot1B/test.py
@@ -0,0 +1,36 @@
+import timeit
+import torch
+
+import asyncio
+from transformers import pipeline, Conversation, AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForCausalLM
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.current_device())
+print(torch.cuda.get_device_name(torch.cuda.current_device()))
+
+model_name = "facebook/blenderbot-1B-distill"
+
+# Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name,
+        torch_dtype=torch.float16, load_in_8bit=True)
+
+
+# Initialize the pipeline
+generator = pipeline("conversational", model=model, tokenizer=tokenizer)
+
+# Asynchronous function to generate response
+async def generate_response_async(input_text):
+    convo = Conversation(input_text)
+    response = await asyncio.to_thread(generator, convo)
+    return response.generated_responses[-1]
+
+# Main function to test the chatbot
+async def main():
+    s = timeit.default_timer()
+    response = await generate_response_async("Hi, how are you?")
+    print(timeit.default_timer()-s)
+    print(response)
+
+# Run the main function
+asyncio.run(main())
