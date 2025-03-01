13e791cfcf8da9a277b6db73e8b1b786cf1a4cab
Hys-01 <hbros2004@gmail.com>
2025-03-01 22:56:17 +1100

    Conversation() objects will handle memory logic, refactor template

--- discordbot.py/memory.py
+++ discordbot.py/memory.py
@@ -1,23 +1,10 @@
 # for multiple users we want individual memory for each
 from model import generator
 from transformers import Conversation
-inputs = []
-responses = []
-def inputOutput(input, chat_history):
-    global inputs, responses
-    convo = Conversation(text=input, past_user_inputs=inputs, generated_responses=responses)
-    response = generator(convo)
+from collections import defaultdict
 
-    response_text = response.generated_responses[-1]
-    
-    # UPDATE MEMORY
-    #   WITHOUT APPENDING TO INPUT AND RESPONSES, IT CANNOT REMEMBER MY NAME
-    #   AFTER APPENDING, IT DOES. CONFIRMED MEMORY IS FUNCTIONAL.
-    inputs.append(input)
-    responses.append(response_text)
-    # FIN UPDATE MEMORY
-    if len(inputs) > 25:
-        inputs.pop(0)
-        responses.pop(0)
-        
-    return response_text
+Conversation()
+conversation_history = defaultdict(Conversation())
+# conversation objects are prebuilt input/output history storage object
+
+
